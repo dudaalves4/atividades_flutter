b53147ba340082825188a0c293287871e01d187b
dudaalves4 <130395819+dudaalves4@users.noreply.github.com>
2023-08-03 10:10:22 -0300

    aula 03/08

---
+++ cardapio_lib/pasta/Class.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Class {
+  final String id;
+  final String titulo;
+  final Color color;
+
+const Class ({
+  required this.id,
+  required this.titulo,
+  required this.color,
+});
+}
---
+++ cardapio_lib/pasta/mock_data.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import './Class.dart';
+
+const mockCategories = [
+  Class(
+    id: 'c1',
+    titulo: 'Italiano',
+    color: Colors.purple,
+  ),
+  Class(
+    id: 'c2',
+    titulo: 'Rápido & Fácil',
+    color: Colors.red,
+  ),
+  Class(
+    id: 'c3',
+    titulo: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  Class(
+    id: 'c4',
+    titulo: 'Alemã',
+    color: Colors.amber,
+  ),
+  Class(
+    id: 'c5',
+    titulo: 'Leve & Saudável',
+    color: Colors.indigo,
+  ),
+  Class(
+    id: 'c6',
+    titulo: 'Exótica',
+    color: Colors.green,
+  ),
+  Class(
+    id: 'c7',
+    titulo: 'Café da Manhã',
+    color: Colors.lightBlue,
+  ),
+  Class(
+    id: 'c8',
+    titulo: 'Asiática',
+    color: Colors.lightGreen,
+  ),
+  Class(
+    id: 'c9',
+    titulo: 'Francesa',
+    color: Colors.pink,
+  ),
+  Class(
+    id: 'c10',
+    titulo: 'Verão',
+    color: Colors.teal,
+  ),
+];
